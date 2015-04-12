16397f433cdf7ac303e304a975ab883c47d841f4
Jack Moxon <jsmoxon@gmail.com>
2015-04-12 14:31:52 -0700

    adding active status to prod

--- gifts/models.py
+++ gifts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
 	"""primary user profile info"""
 	user = models.ForeignKey(User, unique=True)
-
+	active_beta = models.NullBooleanField(default=False, blank=True, null=True)
 	def __unicode__(self):
 		return str(self.user)
 
